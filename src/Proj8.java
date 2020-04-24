64ed490a4a332b799035d111a357ca30cd7b586e
NotZack <zackarynichol@gmail.com>
2020-04-24 15:21:31 -0500

    Completed basic requirements

--- src/Proj8.java
+++ src/Proj8.java
@@ -2,68 +2,126 @@ import java.util.*;
 
 /*******************************************************************
  * Proj8.java* <Zackary Nichol / Friday/3:30>
+ * A student information entering program that allows the user to
+ * input and validate each student's name, username, WID, and gpa.
+ * Then allows the user to perform a search of the entered users
+ * based on their WID.
  *******************************************************************/
 
 public class Proj8 {
+
+    /**
+     * Performs all user input, input validation, and student WID search
+     * functions.
+     * @param args Unused command line arguments
+     */
     public static void main(String[] args) {
 
         Scanner s = new Scanner (System.in);
         ArrayList<Student> studentsList = new ArrayList<>();
 
-        String reply;
-
-        do {
-            reply = "Y";
+        String reply = "Y";
+        while (reply.equalsIgnoreCase("Y")) {
 
             System.out.print("Enter the student's name: ");
             String name = s.nextLine();
-            try {
-                if (name.equals("")) throw new Error("Name is required (Please re-enter)");
-            }
-            catch (Error e) {
-                System.out.println(e.getMessage());
-                continue;
+            while (name.equals("")) {
+                System.out.println("Name is required (Please re-enter)\n");
+                System.out.print("Enter the student's name: ");
+                name = s.nextLine();
             }
 
-            System.out.print("Enter student's user name: ");
+            System.out.print("Enter student's USER name: ");
             String userName = s.nextLine();
-            try {
-                if (userName.equals("")) throw new Error("User name is required (Please re-enter name and user name)");
-            }
-            catch (Error e) {
-                System.out.println(e.getMessage());
-                continue;
+            while (userName.equals("")) {
+                System.out.println("User name is required (Please re-enter)\n");
+                System.out.print("Enter student's USER name: ");
+                userName = s.nextLine();
             }
 
-            System.out.print("Enter student's GPA: ");
-            double gpa = 0;
-            try {
-                gpa = Double.parseDouble(s.nextLine());
-                while (gpa < 0.0 || gpa > 4.0) {
-                    System.out.print("GPA must be between 0.0-4.0 (inclusive). Please re-enter: ");
-                    gpa = Double.parseDouble(s.nextLine());
+            String WID;
+            while (true) {
+                try {
+                    System.out.print("Enter student's WID #: ");
+                    WID = s.nextLine();
+                    if (WID.equals("")) throw new Error();
+                    Integer.parseInt(WID);
+                    if (WID.length() != 9) throw new NumberFormatException();
+                    break;
+                }
+                catch (Error e) {
+                    System.out.println("WID is required (Please re-enter)\n");
+                }
+                catch (NumberFormatException e) {
+                    System.out.println("WID must contain 9-digits - no characters (Please re-enter)\n");
                 }
             }
-            catch (NumberFormatException e) {
-                System.out.println("No characters allowed for GPA...Please re-enter all information");
-                continue;
+
+            double gpa;
+            System.out.print("Enter student's GPA: ");
+            while (true) {
+                try {
+                    gpa = Double.parseDouble(s.nextLine());
+                    if (gpa < 0.0 || gpa > 4.0) {
+                        System.out.println("GPA must be between 0.0-4.0 (inclusive).");
+                        System.out.print("Please re-enter: ");
+                        continue;
+                    }
+                    break;
+                }
+                catch (NumberFormatException e) {
+                    System.out.print("No characters allowed for GPA...\nPlease re-enter: ");
+                }
             }
-
-            studentsList.add(new Student(name, userName, gpa));
-
-            System.out.println("Student added to the arraylist...");
+            Student newStudent = new Student(name, userName, WID, gpa);
+            for (Student student : studentsList) {
+                if (student.equals(newStudent)) {
+                    System.out.println("Student already exists.");
+                    newStudent = null;
+                    break;
+                }
+            }
+            if (newStudent != null) {
+                studentsList.add(newStudent);
+                System.out.println("Student added to the arraylist...");
+            }
             System.out.print("\nAdd another student? ('Y' or 'N'): ");
             reply = s.nextLine();
         }
-        while (reply.equalsIgnoreCase("Y"));
+
+        HashMap<String, Student> studentMap = new HashMap<>();
 
         for (Student student : studentsList) {
-            System.out.println(student.toString() + "\n");
+            System.out.println("\n" + student.toString());
+            studentMap.put(student.getWID(), student);
         }
+        System.out.println("All students displayed.\n");
 
-        System.out.println("Students on the Dean's list");
-        for (Student student : studentsList) {
-            if (student.getGpa() > 3.0) System.out.println(student.getName() + " - " + student.getGpa());
+        System.out.println("Search for a Student by entering Student's WID number\n" +
+                "(or enter \"exit\" to exit the program.)");
+        while (true) {
+            System.out.print("Input Student WID # (or \"exit\"): ");
+            String searchInput = s.nextLine();
+            if (searchInput.equals("")) {
+               System.out.println("Nothing Entered");
+               continue;
+            }
+            else if (searchInput.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            boolean studentFound = false;
+            for (String WID : studentMap.keySet()) {
+                if (WID.equals(searchInput)) {
+                    System.out.println("Student info:\n" + studentMap.get(WID).toString());
+                    studentFound = true;
+                    break;
+                }
+            }
+
+            if (!studentFound) {
+                System.out.println("Student not found");
+            }
         }
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,28 +1,54 @@
+/**
+ * A Student represents a actual physical student at K-State with a name, username, gpa, and WID
+ */
 public class Student {
+
     private String name;
     private String userName;
     private double gpa;
+    private String WID;
 
-    Student(String name, String userName, double gpa) {
+    /**
+     * Creates a new, complete student
+     * @param name The name of the student
+     * @param userName The user name of the student
+     * @param WID The WID of the student
+     * @param gpa The cumulative gpa of the student
+     */
+    Student(String name, String userName, String WID, double gpa) {
         this.name = name;
         this.userName = userName;
+        this.WID = WID;
         this.gpa = gpa;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public double getGpa() {
-        return gpa;
-    }
-
+    /**
+     * @return The email of this student
+     */
     public String getEmail() {
         return userName + "@ksu.edu";
     }
 
+    /**
+     * @return The WID of this student
+     */
+    public String getWID() {
+        return this.WID;
+    }
+
+    /**
+     * A student is equal if their WIDs match.
+     * @return If the students' WIDs matched
+     */
+    public boolean equals(Student otherStudent) {
+        return otherStudent.getWID().equals(this.getWID());
+    }
+
+    /**
+     * @return A string representation of all of this student's variables
+     */
     @Override
     public String toString() {
-        return name + "\n" + getEmail() + "\nGPA: " + gpa;
+        return name + "\nWID: " + WID + "\n" + getEmail() + "\nGPA: " + gpa;
     }
 }
